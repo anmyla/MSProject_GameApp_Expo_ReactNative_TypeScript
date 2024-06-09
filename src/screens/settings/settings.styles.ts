9b4be64e09a4c7012ea72ca3ca35d5fc18fd5acd
anmyla <myla.ariaga@gmail.com>
2024-06-09 22:45:12 +0200

    Settings page UI

--- src/screens/settings/settings.styles.ts
+++ src/screens/settings/settings.styles.ts
@@ -2,9 +2,40 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create ({
 container: {
-    flex: 1,
-    justifyContent: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 40
+
+}, 
+field: {
+    marginBottom: 30
+
 },
+label: {
+    color: '#fff',
+    fontSize: 18,
+    fontFamily: "DeliusUnicase-Bold",
+},
+choices: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 10,
+    marginHorizontal: -5
+},
+choice: {
+    backgroundColor: '#fff',
+    padding: 10,
+    margin: 5
+},
+choiceText: {
+    color: '#000',
+    fontFamily: "DeliusUnicase-Bold",
+}, 
+switch: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+}
+
 
 
 });
--- src/screens/settings/settings.tsx
+++ src/screens/settings/settings.tsx
@@ -1,16 +1,64 @@
-import { View, Text, ScrollView } from 'react-native'
-import React, { ReactElement } from 'react'
-import { GradienBackground } from '../../components'
-import styles from './settings.styles'
+import { View, Text, ScrollView, TouchableOpacity, Switch } from "react-native";
+import React, { ReactElement, useState } from "react";
+import { GradienBackground } from "../../components";
+import styles from "./settings.styles";
 
+export default function Settings(): ReactElement {
+  const [state, setState] = useState(false);
 
-export default function Settings():ReactElement
- {
+  const difficulty = {
+    "1": "Beginner",
+    "3": "Intermediate",
+    "4": "Hard",
+    "-1": "Impossible",
+  };
+
   return (
     <GradienBackground>
-    <ScrollView contentContainerStyle= {styles.container}>
-     
-    </ScrollView>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.field}>
+          <Text style={styles.label}>DIFFICULTY LEVEL</Text>
+          <View style={styles.choices}>
+            {Object.keys(difficulty).map((level) => {
+              return (
+                <TouchableOpacity style={styles.choice} key={level}>
+                  <Text style={styles.choiceText}>
+                    {difficulty[level as keyof typeof difficulty]}
+                  </Text>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </View>
+        <View style={[styles.field, styles.switch]}>
+          <Text style={styles.label}>SOUNDS</Text>
+          <Switch
+            trackColor={{
+              false: "#444348",
+              true: "#ffffff",
+            }}
+            thumbColor={"#705B96"}
+            value={state}
+            onValueChange={() => {
+              setState(!state);
+            }}
+          />
+        </View>
+        <View style={[styles.field, styles.switch]}>
+          <Text style={styles.label}>HAPTICS</Text>
+          <Switch
+            trackColor={{
+              false: "#444348",
+              true: "#ffffff",
+            }}
+            thumbColor={"#705B96"}
+            value={state}
+            onValueChange={() => {
+              setState(!state);
+            }}
+          />
+        </View>
+      </ScrollView>
     </GradienBackground>
-  )
+  );
 }
