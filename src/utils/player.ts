6c559da6d324fc497bbb98e11f6414bd6a854c9a
anmyla <myla.ariaga@gmail.com>
2024-06-09 11:02:08 +0200

    getBestMove edit to get index of the best move

--- src/utils/player.ts
+++ src/utils/player.ts
@@ -1,14 +1,21 @@
 import { BoardProps } from "./types";
 import { isTerminal, getAvailableMoves, printFormattedBoard } from "./board";
 
+
+
 export const getBestMove = (state: BoardProps['squares'], maximizing : boolean, depth = 0): number => {
+const childValues: {[key: string] : string} = {};
+
+const getBestMoveRecursive = (state: BoardProps['squares'], maximizing : boolean, depth = 0): number => {    
     const terminalObject = isTerminal(state);
     if(terminalObject) {
         if(terminalObject.winner === 'X') {
             return 100 -depth;
         } else if(terminalObject.winner=== 'O') {
             return - 100 + depth;
-        }
+        } else {
+            return 0;
+        } 
     };
 
     if(maximizing) {
@@ -16,9 +23,17 @@ export const getBestMove = (state: BoardProps['squares'], maximizing : boolean, 
         getAvailableMoves(state).forEach(index => {
             const child: BoardProps['squares'] = [...state];
             child[index] = 'X';
-            const childValue = getBestMove(child, false, depth + 1);
+            const childValue = getBestMoveRecursive(child, false, depth + 1);
             best = Math.max(best, childValue);
-        }) 
+            if(depth === 0) {
+                childValues[childValue] = childValues[childValue] ? `${childValues[childValue]}, ${index}`: `${index}`;
+            }
+        });
+        if(depth === 0) {
+            const arr = childValues[best].split(',');
+            const rand  = Math.floor(Math.random() * arr.length);
+            return parseInt(arr[rand]);
+        } 
         return best;
     };
     if(!maximizing) {
@@ -28,7 +43,19 @@ export const getBestMove = (state: BoardProps['squares'], maximizing : boolean, 
             child[index] = 'O';
             const childValue = getBestMove(child, true, depth + 1);
             best = Math.min(best, childValue);
-        }) 
+            if(depth === 0) {
+                childValues[childValue] = childValues[childValue] ? `${childValues[childValue]}, ${index}`: `${index}`;
+            }
+        }); 
+        if(depth === 0) {
+            const arr = childValues[best].split(',');
+            const rand  = Math.floor(Math.random() * arr.length);
+            return parseInt(arr[rand]);
+        }
         return best;
     };
+
+    return 0;
+};
+return getBestMoveRecursive(state, maximizing, depth);
 };
