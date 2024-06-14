3a9ec1e78f9aadcfc48bc4fa3d3b3ac6a8aea2ab
anmyla <myla.ariaga@gmail.com>
2024-06-14 20:43:29 +0200

    login page draft

--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -1,24 +1,50 @@
-import React, { ReactElement, useRef } from "react";
+import React, { ReactElement, useRef, useState } from "react";
 import { ScrollView, TextInput as NativeTextInput } from "react-native";
 import styles from "./login.styles";
-import { GradienBackground, TextInput} from "../../components";
+import { GradienBackground, TextInput, MyButton} from "../../components";
 
 export default function Login(): ReactElement {
     const passwordRef = useRef<NativeTextInput | null>(null);
-  return (
+    const [form, setForm] = useState({
+        username: '',
+        password: ''
+    })
+    const [loading, setLoading] = useState(false);
+
+    const setFormInput = (key : keyof typeof form, value: string) => {
+        setForm({...form, [key]: value})
+    }
+
+    const login = () => {
+        setLoading(true);
+        const {username, password} = form;
+        console.log(username, password)
+        
+        setLoading(false);
+    }
+
+    return (
     <GradienBackground>
       <ScrollView contentContainerStyle={styles.container}>
-        <TextInput 
+        <TextInput
+        value={form.username} 
+        onChangeText={(value) => {
+            setFormInput('username', value)}}
         returnKeyType="next" 
         placeholder="Username"
         onSubmitEditing={()=>{ passwordRef.current?.focus()
 
         }}/>
         <TextInput 
+        value={form.password}
+        onChangeText={(value) => {
+            setFormInput('password', value)}}
         ref={passwordRef}
         returnKeyType="done" 
         secureTextEntry 
         placeholder="Password"/>
+        <MyButton title={"Log In"} onPress={login}/>
+            
       </ScrollView>
     </GradienBackground>
 
